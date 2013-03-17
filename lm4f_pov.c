1b4800aad8e65238a589998c5623ebb3d786496c
Kristian Nielsen <knielsen@knielsen-hq.org>
2013-03-17 14:29:54 +0100

    Add 20MHz PWM output for TLC5940 GSCLK.

--- lm4f_pov.c
+++ lm4f_pov.c
@@ -7,10 +7,36 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
+#include "driverlib/timer.h"
+
 
 #define LED_RED GPIO_PIN_1
 #define LED_BLUE GPIO_PIN_2
 #define LED_GREEN GPIO_PIN_3
+
+/* Configure Timer1B to output a PWM signal for GSCLK. */
+static void
+setup_pwm_GSCLK(void)
+{
+  /* Enable the timer. */
+  ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+  /* Enable the GPIO module for the timer pin. */
+  ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+  /* Configure PB5 to be I/O pin for timer 1 B. */
+  ROM_GPIOPinConfigure(GPIO_PB5_T1CCP1);
+  /* Configure PB5 (output etc. (?)) */
+  ROM_GPIOPinTypeTimer(GPIO_PORTB_BASE, GPIO_PIN_5);
+  ROM_TimerConfigure(TIMER1_BASE, TIMER_CFG_16_BIT_PAIR | TIMER_CFG_B_PWM);
+  /*
+    Set timer1B start and compare values.
+    High at 3, low at 1 -> 80MHz/4 = 20MHz clock.
+  */
+  ROM_TimerLoadSet(TIMER1_BASE, TIMER_B, 3);
+  ROM_TimerMatchSet(TIMER1_BASE, TIMER_B, 1);
+  /* Start the timer. */
+  ROM_TimerEnable(TIMER1_BASE, TIMER_B);
+}
+
 
 int main()
 {
@@ -18,9 +44,13 @@ int main()
   ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL |
                      SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
 
+  setup_pwm_GSCLK();
+
+  /* Configure LED GPIOs. */
   ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
   ROM_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, LED_RED|LED_BLUE|LED_GREEN);
 
+  /* Configure serial. */
   ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
   ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
   ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
